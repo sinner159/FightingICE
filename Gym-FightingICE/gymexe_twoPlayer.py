cc290dd3318459b79ae2876455dfe1c26878d1c7
sinner159 <rsinner159@gmail.com>
2022-05-05 11:51:03 -0400

    finally able to debug two_player

--- Gym-FightingICE/gymexe_twoPlayer.py
+++ Gym-FightingICE/gymexe_twoPlayer.py
@@ -6,6 +6,7 @@ from gym_fightingice.envs.Machete import Machete
 from gym_fightingice.envs.fightingice_env_twoplayer import play_thread
 import time
 import os
+from gym_fightingice.envs.gym_ai import GymAI
 from gym_fightingice.testAI import TestAI
 
 def p_thread1(env,p1,p2):
@@ -24,12 +25,12 @@ def p_thread2(env,p1,p2):
         while not done:
             new_obs, reward, done, _ = env.step(38)
 
-cwd = f"{os.getcwd()}\\Gym-FightingICE\\"
+cwd = f"{os.getcwd()}"
 env1 = gym.make("FightingiceDataTwoPlayer-v0", java_env_path=cwd,port=4242, freq_restart_java=3)
 p2_server = env1.build_pipe_and_return_p2()
 env2 = gym.make("FightingiceDataTwoPlayer-v0", java_env_path=cwd,port=4242,p2_server=p2_server)
-t1 = Thread(target=p_thread1, name="play_thread1", args=(env1,TestAI,Machete, ))
-t2 = Thread(target=p_thread2, name="play_thread2", args=(env2,TestAI,Machete, ))
+t1 = Thread(target=p_thread1, name="play_thread1", args=(env1,TestAI,GymAI, ))
+t2 = Thread(target=p_thread2, name="play_thread2", args=(env2,TestAI,GymAI, ))
 t1.start()
 time.sleep(10)
 t2.start()
