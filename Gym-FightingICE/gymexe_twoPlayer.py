5d4a4da4c73becd744d99c46a7eb7b199cbc8490
sinner159 <rsinner159@gmail.com>
2022-05-05 11:31:04 -0400

    adding testAI

--- Gym-FightingICE/gymexe_twoPlayer.py
+++ Gym-FightingICE/gymexe_twoPlayer.py
@@ -2,15 +2,15 @@ import gym
 import gym_fightingice
 import random
 from threading import Thread
+from gym_fightingice.envs.Machete import Machete
 from gym_fightingice.envs.fightingice_env_twoplayer import play_thread
 import time
 import os
-
-from gym_fightingice.envs.gym_ai import GymAI
+from gym_fightingice.testAI import TestAI
 
 def p_thread1(env,p1,p2):
     while True:
-        obs = env.reset(p1=p1,p2=p2)
+        obs = env.reset(p1=p1)
 
         done = False
         while not done:
@@ -18,7 +18,7 @@ def p_thread1(env,p1,p2):
 
 def p_thread2(env,p1,p2):
     while True:
-        obs = env.reset(p1=p1,p2=p2)
+        obs = env.reset(p2=p2)
 
         done = False
         while not done:
@@ -27,8 +27,8 @@ def p_thread2(env,p1,p2):
 env1 = gym.make("FightingiceDataTwoPlayer-v0", java_env_path=os.getcwd(),port=4242, freq_restart_java=3)
 p2_server = env1.build_pipe_and_return_p2()
 env2 = gym.make("FightingiceDataTwoPlayer-v0", java_env_path=os.getcwd(),port=4242,p2_server=p2_server)
-t1 = Thread(target=p_thread1, name="play_thread1", args=(env1,GymAI,GymAI, ))
-t2 = Thread(target=p_thread2, name="play_thread2", args=(env2,GymAI,GymAI, ))
+t1 = Thread(target=p_thread1, name="play_thread1", args=(env1,TestAI,Machete, ))
+t2 = Thread(target=p_thread2, name="play_thread2", args=(env2,TestAI,Machete, ))
 t1.start()
 time.sleep(10)
 t2.start()
