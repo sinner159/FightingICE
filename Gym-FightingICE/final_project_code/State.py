c9b11311a7d4f87032564761eeb512deb9d3839d
sinner159 <rsinner159@gmail.com>
2022-05-06 18:17:04 -0400

    added characterDataWrapper

--- Gym-FightingICE/final_project_code/State.py
+++ Gym-FightingICE/final_project_code/State.py
@@ -1,3 +1,4 @@
+from final_project_code.wrappers.CharacterDataWrapper import CharacterDataWrapper
 from .action import ALL_USEFUL_ACTIONS
 from final_project_code.wrappers.SimulatorWrapper import SimulatorWrapper
 from py4j.java_gateway import get_method
@@ -10,30 +11,36 @@ class State():
     def __init__(self, frameData = None):
         self.frameData = frameData
 
+        p1 = self.frameData.getCharacter(False)
+        p2 = self.frameData.getCharacter(True)
+        self.p1 = CharacterDataWrapper(p1)
+        self.p2 = CharacterDataWrapper(p2)
+
+        self.rem = self.frameData.getRemainingFramesNumber()
+        self.distanceY = self.frameData.getDistanceX()
+        self.distanceY = self.frameData.getDistanceY()
+        self.framesSinceStart = self.frameData.getFramesNumber()
+        self.dequeAtkData = self.frameData.getProjectiles()
+        self.dequeAtkDataP1 = self.frameData.getProjectilesByP1()
+        self.dequeAtkDataP2 = self.frameData.getProjectilesByP2()
+        self.timeSec = self.frameData.getRemainingTime()
+        self.timeMill = self.frameData.getRemainingTimeMilliseconds()
+        self.roundNum = self.frameData.getRound()
+        self.timeRemaining = self.frameData.getRemainingTimeMilliseconds()
+
+
+
+
     def getLegalActions(self):
+
+        
         return ALL_USEFUL_ACTIONS
         
     def isGameOver(self):
         if self.frameData.getEmptyFlag():
             return False
 
-        rem = self.frameData.getRemainingFramesNumber()
-        p1 = self.frameData.getCharacter(False)
-        p2 = self.frameData.getCharacter(True)
-        distanceY = self.frameData.getDistanceX()
-        distanceY = self.frameData.getDistanceY()
-        framesSinceStart = self.frameData.getFramesNumber()
-        dequeAtkData = self.frameData.getProjectiles()
-        dequeAtkDataP1 = self.frameData.getProjectilesByP1()
-        dequeAtkDataP2 = self.frameData.getProjectilesByP2()
-        timeSec = self.frameData.getRemainingTime()
-        timeMill = self.frameData.getRemainingTimeMilliseconds()
-        roundNum = self.frameData.getRound()
-        p1Hp = p1.getHp()
-        p2Hp = p2.getHp()
-        timeRemaining = self.frameData.getRemainingTimeMilliseconds()
-
-        return p1Hp <= 0 or p2Hp <= 0 or timeRemaining <= 0 
+        return self.p1.hp <= 0 or self.p2.hp <= 0 or self.timeRemaining <= 0 
 
 
     def move(self, action):
